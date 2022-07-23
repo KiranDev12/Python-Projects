7c94ef22b8054baa756cbbced8bbbd6343577c29
Kiran Dev K <kirandev.edu@gmail.com>
2022-07-23 13:29:57 +0530

    Projects added

---
+++ Snake-Game/main.py
@@ -0,0 +1,21 @@
+from turtle import Screen, Turtle
+from snake import Snake
+import time
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake game")
+screen.tracer(0)
+
+snake = Snake()
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+
+    snake.move()
+
+
+screen.exitonclick()
